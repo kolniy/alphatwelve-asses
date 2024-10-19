4bfe51cd614a32a8a044dba370a252a820e8eab5
kolaniyi <kolaniyi3@gmail.com>
2024-10-19 21:38:38 +0100

    implemented basic dashboard page

---
+++ js/chart.js
@@ -0,0 +1,63 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const ctx = document.getElementById("reg-chart").getContext("2d");
+  let ticksColor = "#64748b";
+  const chartData = {
+    labels: [
+      "Jan",
+      "Fab",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ],
+    datasets: [
+      {
+        data: [700, 950, 780, 420, 1000, 590, 830, 380, 810, 620, 970, 600],
+        backgroundColor: "#8576FF",
+        borderWidth: 1,
+      },
+    ],
+  };
+  // Define the chart options (optional)
+  const chartOptions = {
+    plugins: {
+      legend: {
+        display: false,
+      },
+    },
+    scales: {
+      x: {
+        grid: {
+          drawTicks: false,
+        },
+        ticks: {
+          color: ticksColor,
+          fontSize: "80",
+          fontWeight: "400",
+          margin: "20",
+        },
+      },
+      y: {
+        beginAtZero: true,
+        ticks: {
+          stepSize: 200, // Increment of 200
+          color: ticksColor,
+        },
+        grid: {
+          drawTicks: false,
+        },
+      },
+    },
+  };
+  const myBarChart = new Chart(ctx, {
+    type: "bar",
+    data: chartData,
+    options: chartOptions,
+  });
+});
---
+++ js/slidder.js
@@ -0,0 +1,39 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll(".slide");
+const totalSlides = slides.length;
+
+function showSlide(index) {
+  // Loop back to the first slide if current index is less than 0
+  // Loop to the last slide if current index exceeds total slides
+  currentSlide = (index + totalSlides) % totalSlides;
+
+  // Move the slides container
+  const offset = -currentSlide * 600; // Width of the slides
+  document.querySelector(
+    ".carousel-images"
+  ).style.transform = `translateX(${offset}px)`;
+
+  // Update indicators
+  updateIndicators();
+}
+
+// Function to change the slide
+function changeSlide(direction) {
+  showSlide(currentSlide + direction);
+}
+
+// Function to update indicators
+function updateIndicators() {
+  const indicators = document.querySelectorAll(".indicator");
+  indicators.forEach((indicator, index) => {
+    indicator.classList.toggle("active", index === currentSlide);
+  });
+}
+
+// Auto-slide functionality (optional)
+setInterval(() => {
+  changeSlide(1);
+}, 3000); // Change slide every 3 seconds
+
+// Initialize indicators on page load
+updateIndicators();
