cfc8d7913000baf5e157cd31c4d516d715a073e4
kolaniyi <kolaniyi3@gmail.com>
2024-10-21 17:30:55 +0100

    completed dashboard on desktop

--- js/index.js
+++ js/index.js
@@ -6,7 +6,31 @@ document.addEventListener("DOMContentLoaded", function () {
 
     for (const key in eventItem) {
       const cell = document.createElement("td");
-      cell.textContent = eventItem[key];
+
+      if (key.toLowerCase() === "status") {
+        const statusSpan = document.createElement("span");
+
+        const dotIcon = document.createElement("i");
+        dotIcon.classList.add("fas");
+        dotIcon.classList.add("fa-circle");
+
+        const textSpan = document.createElement("span");
+        textSpan.textContent = eventItem[key];
+
+        statusSpan.appendChild(dotIcon);
+        statusSpan.appendChild(textSpan);
+
+        if (eventItem[key].toLowerCase() === "completed") {
+          statusSpan.classList.add("status-span", "completed");
+        } else {
+          statusSpan.classList.add("status-span", "inprogress");
+        }
+
+        cell.appendChild(statusSpan);
+      } else {
+        cell.textContent = eventItem[key];
+      }
+
       row.appendChild(cell);
     }
 
